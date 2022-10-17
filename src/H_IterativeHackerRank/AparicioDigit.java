43773eac921eb73a0f00adb22470467f3c746eb4
Daruuu <thedaruny16@gmail.com>
2022-10-17 20:10:29 +0200

    adding in H 6 exercises

--- src/H_IterativeHackerRank/AparicioDigit.java
+++ src/H_IterativeHackerRank/AparicioDigit.java
@@ -5,14 +5,7 @@ import java.util.Scanner;
 public class AparicioDigit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        char digit = sc.next().charAt(0);
-        int contador = 0;
-        while (digit < 10){
-            for (int i = 0; i < 0; i++) {
 
-            }
-        }
 
     }
 }
