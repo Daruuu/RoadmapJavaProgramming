85ae5cb54b1bbe059ba9485e9085d3710b1b0933
Daruuu <thedaruny16@gmail.com>
2022-11-17 00:05:59 +0100

    exercises arrays 2D 4-6

--- src/J_ExercisisMatrices/Exercise04.java
+++ src/J_ExercisisMatrices/Exercise04.java
@@ -1,14 +1,31 @@
 package J_ExercisisMatrices;
 
 import java.util.Scanner;
-//4. Fer un programa que llegeixi una taula numèrica real i escrigui la taula transposada. La taula transposada s'obté canviant les files per les columnes.
+
+//4. Fer un programa que llegeixi una taula numèrica real i escrigui la taula transposada.
+// La taula transposada s'obté canviant les files per les columnes.
 public class Exercise04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] matriz = new int[][];
-        for (int i = 0; i < ; i++) {
-
+        int[][] matriz = new int[n][n];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = (int) (Math.random() * 100);
+            }
+        }
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] +" ");
+            }
+            System.out.println(" ");
+        }
+        System.out.println();
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[j][i] +" ");
+            }
+            System.out.println(" ");
         }
     }
 }
---
+++ src/J_ExercisisMatrices/Exercise05.java
@@ -0,0 +1,8 @@
+package J_ExercisisMatrices;
+//5. Donat un nombre enter major que zero, crea la matriu identitat de dimensió el nombre enter donat. (http://ca.wikipedia.org/wiki/Matriu_identitat)
+
+public class Exercise05 {
+    public static void main(String[] args) {
+
+    }
+}
