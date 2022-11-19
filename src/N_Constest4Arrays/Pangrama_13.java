607333292783782a8d7311edd3ff9bfbabce1cc8
Daruuu <thedaruny16@gmail.com>
2022-11-19 01:22:33 +0100

    from windows

---
+++ src/N_Constest4Arrays/Pangrama_13.java
@@ -0,0 +1,13 @@
+package N_Constest4Arrays;
+
+import java.util.Scanner;
+
+public class Pangrama_13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().toLowerCase();
+        str = str.replaceAll(" ","");
+        String alfabeto = "abcdefghijklmnopqrstuvwxyz";
+
+    }
+}
--- src/N_Constest4Arrays/PosicionAlfabeto_11.java
+++ src/N_Constest4Arrays/PosicionAlfabeto_11.java
@@ -6,16 +6,12 @@ public class PosicionAlfabeto_11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine().toLowerCase();
+        str = str.replaceAll(" ","");
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
         // 122-97=25
         //65
-        int iterador = 0;
-        for (int i = 'a'; i <= 'z'; i++) {
-            System.out.print(i + " ");
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(alphabet.indexOf(str.charAt(i)) + " ");
         }
-        for (int i = 0; i <= 26; i++) {
-            if ()
-        }
-
-
     }
 }
