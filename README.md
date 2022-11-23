f98f755b202997656f875dd91a39a92e65a508c4
Daruuu <thedaruny16@gmail.com>
2022-11-23 21:37:06 +0100

    multiple 3 5 nuwe

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Inside of **directory** `src` I'll be adding the exercises that I'm doing and sa
 Within each directory you can see a description of the exercises statements or a link to [HackerRank](https://www.hackerrank.com/) where you can see the different statements.
 All the exercises are in __spanish__ or __catalan__.
 
+![image setup](../../../Downloads/backgroundRepo.jpg)
--- src/SCRUM/WadRas.java
+++ src/SCRUM/WadRas.java
@@ -1,10 +1,12 @@
 package SCRUM;
 
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class WadRas {
     public static void main(String[] args) {
-
+/*
         // MENU PROGRAMA
         Scanner sc = new Scanner(System.in);
         boolean sortir = false;
@@ -22,7 +24,6 @@ public class WadRas {
             switch (opcio) {
                 case 1:
                     System.out.println("HAS SEL·LECCIONAT L'OPCIÓ 1");
-
                     break;
                 case 2:
                     System.out.println("HAS SEL·LECCIONAT L'OPCIÓ 2");
@@ -37,37 +38,69 @@ public class WadRas {
                     System.out.println("HAS DE SEL·LECCIONAR UNA DE LES 4 OPCIONS");
             }
         }
-
+ */
 
         //FICHA TECNICA PRESAS
 
-        //                              nombre,             edad, sentencia, condena restante, tipo grado
-//        String[] fichaTecnicaPresa1 = {"Juana Nascimento", "29", "20", "11", "2"};
-//        String[] fichaTecnicaPresa2 = {"Giovana Libert", "42", "10", "5", "1"};
-//        String[] fichaTecnicaPresa3 = {"Maria Lisa", "33", "14", "3", "3"};
-//        String[] fichaTecnicaPresa4 = {"Marta", "39", "4", "7", "2"};
+        //                            nombre,             edad, sentencia, condena restante, tipo grado
+        /*
+        String[] fichaTecnicaPresa1 = {"Juana Nascimento", "29", "20", "11", "2"};
+        String[] fichaTecnicaPresa2 = {"Giovana Libert", "42", "10", "5", "1"};
+        String[] fichaTecnicaPresa3 = {"Maria Lisa", "33", "14", "5", "3"};
+        String[] fichaTecnicaPresa4 = {"Marta", "39", "4", "7", "2"};
+         */
+
 
         //filas = 4
         //columnas = 5
         String[][] fichasTecnicas = {
                 {"Juana Nascimento", "29", "20", "11", "2"},
                 {"Giovana Libert", "42", "10", "5", "1"},
-                {"Maria Lisa", "33", "14", "3", "3"},
-                {"Marta", "39", "4", "7", "2"}
+                {"Maria Lisa", "33", "14", "6", "3"},
+                {"Marta", "39", "4", "7", "3"}
         };
+        boolean encontrado = false;
+        String grado = "3";
+        for (int i = 0; i < fichasTecnicas.length && !encontrado; i++) {
+            for (int j = 0; j < fichasTecnicas[i].length && !encontrado; j++) {
+                if (fichasTecnicas[i][j].equals(grado)){
+                    System.out.println(fichasTecnicas[i][j]);
+                    encontrado = true;
+
+                }
+            }
+        }
+
+        System.out.println("************");
+        for (String[] filas : fichasTecnicas) {
+            for (String columnas : filas) {
+                System.out.print(columnas + " ");
+            }
+            System.out.println();
+        }
+
 
         // GRADOS DE CADA PRESA
         //presas que tienen 3er GRADO y que tienen menos de 9 ANYOS de prision o no
-
+        /*
+        String grade = "3";
+        boolean found = false;
+        for (int i = 0; i < fichaTecnicaPresa1.length && !found; i++) {
+            if (grade.equals(fichaTecnicaPresa1[i])) {
+                found = true;
+                System.out.println("tipo de grado: " + fichaTecnicaPresa1[i]);
+            }
+        }
+         */
 
 
         /*
-        //**************FICHA TECNICA********************
         ArrayList<String> infoPresaName = new ArrayList<>();
 
         infoPresaName.add("Juana Nascimento");
         infoPresaName.add("Giovana Libert");
         infoPresaName.add("Maria Lisa");
+
 
         ArrayList<Integer> infoPresaAge = new ArrayList<>();
 
@@ -90,15 +123,9 @@ public class WadRas {
         ArrayList<Integer> infoPresaGrade = new ArrayList<>();
 
         infoPresaGrade.add(2);
+        infoPresaGrade.add(1);
         infoPresaGrade.add(3);
-        infoPresaGrade.add(3);
-
-        for (String s : infoPresaName) {
-            System.out.println(s + " ");
-        }
-
          */
-
 
     }
 }
