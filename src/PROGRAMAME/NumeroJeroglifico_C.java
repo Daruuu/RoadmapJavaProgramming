365c7ebd1cd4498124109411416b8febf6cab12d
Daruuu <thedaruny16@gmail.com>
2022-12-25 00:53:13 +0100

    adding pdf programame

--- src/PROGRAMAME/NumeroJeroglifico_C.java
+++ src/PROGRAMAME/NumeroJeroglifico_C.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 
 public class NumeroJeroglifico_C {
 
-    public static void numeroAChar(){
+    public static void numeroAChar() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        while (n != 0){
-
+        while (n != 0) {
             n = sc.nextInt();
         }
     }
