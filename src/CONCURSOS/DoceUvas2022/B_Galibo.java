9de13323deccefee102dce857f30d39bd69934a3
Daruuu <thedaruny16@gmail.com>
2023-01-05 00:59:26 +0100

    add 12 uvas , fizzbuzz

---
+++ src/CONCURSOS/DoceUvas2022/B_Galibo.java
@@ -0,0 +1,17 @@
+package CONCURSOS.DoceUvas2022;
+
+import java.util.Scanner;
+
+public class B_Galibo {
+    // numero de puentes
+    // numero de carriles
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n != 0){
+            int numeroDePuentes = sc.nextInt();
+            int numeroCarriles;
+            n =sc.nextInt();
+        }
+    }
+}
