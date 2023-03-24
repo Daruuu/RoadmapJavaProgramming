9a48b1827d9b81db6fdcc532060d7af96ff59128
Daruuu <thedaruny16@gmail.com>
2023-03-24 21:55:44 +0100

    scrum fase 3

--- src/scrum/sprint3/WadRasUpdate.java
+++ src/scrum/sprint3/WadRasUpdate.java
@@ -1,55 +1,55 @@
-package scrum.uf2Final;
+package scrum.sprint3;
 
 import java.util.Scanner;
 
-public class WadRas {
+public class WadRasUpdate {
     public static void main(String[] args) {
-
+        WadRasUpdate wadRasUpdate = new WadRasUpdate();
+        wadRasUpdate.menuWadRas();
     }
 
     public void menuWadRas() {
         Scanner sc = new Scanner(System.in);
         boolean salir = false;
-        Reclusas.establecerEstadoSalidaPatioReclusas();
+
         while (!salir) {
             int opcion = opcionesMenuWadRas(sc);
             switch (opcion) {
                 case 1:
                     System.out.println("\033[36mSELECCIONASTE LA OPCION 1:\033[0m");
                     linea();
-                    Reclusas.mostrarFichaTecnica();
+
                     linea();
                     break;
                 case 2:
                     System.out.println("\033[36mSELECCIONASTE LA OPCION 2:\033[0m");
                     linea();
-                    System.out.print("Introduce numero de reclusas nuevas: ");
-                    int numReclusas = sc.nextInt();
-                    Reclusas.agregarReclusasNuevas(numReclusas);
-                    Reclusas.listarReclusasNuevas();
+
                     linea();
                     break;
                 case 3:
                     System.out.println("\033[36mSELECCIONASTE LA OPCION 3:\033[0m");
                     linea();
-                    Reclusas.mostrarReclusasPatioDisponible();
+
                     linea();
                     break;
                 case 4:
                     System.out.println("\033[36mSELECCIONASTE LA OPCION 4:\033[0m");
                     linea();
-                    ControlReclusas.listarReclusasHorarioCorrecto();
-                    linea();
-                    ControlReclusas.listarReclusasHorarioIncorrecto();
+                    System.out.println("Listado de las Reclusas");
+
                     linea();
                     break;
                 case 5:
                     System.out.println("\033[36mSELECCIONASTE LA OPCION 5:\033[0m");
                     linea();
-                    ControlReclusas.crearVisitasReclusas();
-                    linea();
+                    System.out.println("RECLUSAS QUE TIENEN VISITA:");
+
                     break;
                 case 6:
+
+                    break;
+                case 7:
                     System.out.println("----------FIN DEL PROGRAMA----------");
                     salir = true;
                     break;
@@ -61,9 +61,9 @@ public class WadRas {
 
     public static int opcionesMenuWadRas(Scanner sc) {
         System.out.print("\033[35mGESTION DE LA PRISION WADRAS\033[0m\n" +
-                "[1]- FICHAS ACTUALES DE LAS RECLUSAS\n" +
+                "[1]- LISTAR FICHAS RECLUSAS\n" +
                 "[2]- INTRODUCIR NUEVAS RECLUSAS\n" +
-                "[3]- RECLUSAS DE 3R GRADO QUE PUEDEN SALIR\n" +
+                "[3]- LISTAR RECLUSAS DE 3R GRADO QUE PUEDEN SALIR\n" +
                 "[4]- CONTROL ENTRADA Y SALIDA DE LAS RECLUSAS\n" +
                 "[5]- CONTROL DE VISITAS FAMILIARES/AMIGOS DE LAS RECLUSAS\n" +
                 "[6]- FINALIZA PROGRAMA\n" +
@@ -74,10 +74,4 @@ public class WadRas {
     public static void linea() {
         System.out.println("\033[36m--------------------------------\033[0m");
     }
-
-    void leerArchivoCSV(){
-
-    }
-
-
 }
